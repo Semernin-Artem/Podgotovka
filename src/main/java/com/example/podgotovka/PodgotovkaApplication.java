1cd282209b53d905b0b8648b08bdad5c9f8df7e6
unknown <Student@305-3.spobip-bel.ru>
2024-02-03 14:19:09 +0300

    initial

---
+++ src/main/java/com/example/podgotovka/PodgotovkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.podgotovka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodgotovkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodgotovkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/podgotovka/PodgotovkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.podgotovka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodgotovkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
